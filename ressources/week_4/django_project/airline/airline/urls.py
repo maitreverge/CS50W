4ae8c3dbe4475e5a4f0796c68bbecc59ba42afce
“maitreverge_asahi_linux” <verge.f@gmail.com>
2025-02-09 20:50:22 +0100

    push

--- ressources/week_4/django_project/airline/airline/urls.py
+++ ressources/week_4/django_project/airline/airline/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("flights/", include("flights.urls")),
+    path("flights/", include("flights.urls")),
 ]
 
 
