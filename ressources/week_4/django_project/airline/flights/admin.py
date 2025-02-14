da0669122d5781445040daebb70700df80600881
maitreverge <verge.f@gmail.com>
2025-02-14 15:14:44 +0100

    reset week 4

--- ressources/week_4/django_project/airline/flights/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Flight, Airport, Passenger
-
-# Those two lines tells Django that I want to use those two models with the admin app
-admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
