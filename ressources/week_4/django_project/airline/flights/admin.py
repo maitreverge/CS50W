69002d7738dd1fbff6d667c6cc2c4136a806f016
maitreverge <verge.f@gmail.com>
2025-02-17 15:28:58 +0100

    init work on authentification

--- ressources/week_4/django_project/airline/flights/admin.py
+++ ressources/week_4/django_project/airline/flights/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from .models import Flight, Airport, Passenger
 
 # Those two lines tells Django that I want to use those two models with the admin app
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+# Horizontal filter gives us the ability to drop something from
+# the left to the right
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
